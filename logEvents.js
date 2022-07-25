b462d9ba949eaf0cda0b031666559aa2172b11cb
Ufuk Balaban <ufukkblbnn@gmail.com>
2022-07-25 12:12:45 +0300

    ..

---
+++ logEvents.js
@@ -0,0 +1,37 @@
+console.log("test!");
+
+//**npm i nodemon -g** => it monitors your files and when you save it automatically restarts the server so we're not always have to restart
+//**npm init **/
+//*npm i uuid */ =>  it allow us to generate ids different for each enttry
+ 
+
+const {format} = require('date-fns');
+const {v4:uuid} = require('uuid');
+
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const logEvents = async (message) => {
+    const dateTime = `${format(new Date(),'yyyyMMdd\tHH:mm:ss')}`
+    const logItem =  `${dateTime}\t${uuid()}\t${message}\n`
+    console.log(logItem)
+    try {
+        if(!fs.existsSync(path.join(__dirname,'logs')))
+        {
+            await fsPromises.mkdir(path.join(__dirname,'logs'));
+        }
+
+        fsPromises.appendFile(path.join(__dirname,'logs','eventLog.txt'), logItem);
+    
+    } catch (err) {
+        
+        console.log(error);
+    }
+}
+
+module.exports = logEvents;
+
+// console.log(format(new Date(),'yyyyMMdd\tHH:mm:ss'))
+
+// console.log(uuid())
